9b17e23302d96fc00fa2a4275be0ae3aa452eefb
Brian Richter <brianorwhatever@gmail.com>
2021-02-25 10:06:33 -0800

    updates

--- boot-av1.sh
+++ boot-av1.sh
@@ -1,5 +1,9 @@
-oc delete all,pvc --selector template=av1
+# oc delete dc,pvc --selector template=av1
 
 oc process -f av1-template.yaml --param-file=params/aviary.chat.params | oc apply -f - && \
 oc process -f av1-template.yaml --param-file=params/aviary.icu.params | oc apply -f - && \
-oc process -f av1-template.yaml --param-file=params/aviary.one.params | oc apply -f -
+oc process -f av1-template.yaml --param-file=params/aviary.one.params | oc apply -f - && \
+oc process -f av1-template.yaml --param-file=params/aviary.shop.params | oc apply -f - && \
+oc process -f av1-template.yaml --param-file=params/aviary.fun.params | oc apply -f - && \
+oc process -f av1-template.yaml --param-file=params/aviary.institute.params | oc apply -f - && \
+oc process -f av1-template.yaml --param-file=params/aviary.green.params | oc apply -f -
--- reboot.sh
+++ reboot.sh
@@ -1,5 +1,4 @@
-oc delete all,pvc --selector template=aca-py
+oc delete all,pvc --selector template=av1
 
 oc process -f av1-template.yaml --param-file=params/dev1.env| oc apply -f - && \
-oc process -f av1-template.yaml --param-file=params/dev2.env| oc apply -f - && \
-oc process -f av1-template.yaml --param-file=params/aviary-chat.env | oc apply -f -
+oc process -f av1-template.yaml --param-file=params/dev2.env| oc apply -f -
